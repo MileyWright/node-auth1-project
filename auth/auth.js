8a41c5676cb7e632cebd2bae42578d107b0c8fbc
Miley Wright <dishaliekwright@yahoo.com>
2020-02-12 00:48:41 -0500

    registering a user complete

--- auth/auth.js
+++ auth/auth.js
@@ -1,12 +1,24 @@
 const express = require('express');
 
+const bcrypt = require('bcrypt');
+
 const db = require('./auth-modals');
 
-const router = express.Router;
+const router = express.Router();
 
 // //POST /api/register
 router.post('/register', (req, res) => {
-
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10)
+    user.password = hash;
+    db.add(user)
+    .then(user => {
+        res.status(201).json(`Posted ${user}`)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: 'Could not register user'})
+    })
 })
 
 // // POST /api/login
