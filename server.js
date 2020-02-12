455f16f428bce221eecb40fcbb758d6d905d3480
Miley Wright <dishaliekwright@yahoo.com>
2020-02-11 23:17:03 -0500

    server up and running

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// const Auth = require('./Auth/auth');
+
+const server = express();
+
+server.use(express.json());
+// server.use('/api', Auth);
+
+module.exports = server;
