f63c44651851836fefc545c7803a3fc98ce307cc
Miley Wright <dishaliekwright@yahoo.com>
2020-02-14 21:21:47 -0500

    set up express-session

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 const Auth = require('./Auth/auth');
 
 const server = express();
+
+server.use(
+    session({
+        name: 'node-auth-project',
+        secret: 'shh..this is a secret',
+        httpOnly: true,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60,  // = 1 hours
+            secure: false
+        }
+    })
+)
 
 server.use(express.json());
 server.use('/api', Auth);
