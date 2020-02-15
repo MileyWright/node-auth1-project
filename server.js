050f8da1936a2fad08a316f91b7a4015fab0e1b5
Miley Wright <dishaliekwright@yahoo.com>
2020-02-14 20:57:50 -0500

    installed dependencies

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const helmet = require('helmet');
+const cors = require('cors');
 const Auth = require('./Auth/auth');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api', Auth);
-
+server.use(helmet);
+server.use(cors());
 module.exports = server;
